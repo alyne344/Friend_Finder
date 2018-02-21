1f086d6fa301a90c6994e914ca7fb5a895bc6cd2
Amy Lyne <amy.lyne92@gmail.com>
2018-02-20 18:15:24 -0700

    photo links added

--- app/data/people.js
+++ app/data/people.js
@@ -1,72 +1,72 @@
 var people = [
     {
       name: 'Angelina Jolie',
-      photo: 'https://goo.gl/HjV3wx',
+      photo: 'http://bit.ly/2GvWvLi',
       scores: [ '2', '1', '3', '3', '4', '4', '3', '4', '2', '2', '4', '5' ] 
     },
     {
       name: 'Emma Watson',
-      photo: 'https://goo.gl/A5Qug3',
+      photo: 'http://bit.ly/2opBvib',
       scores: [ '2', '1', '2', '4', '2', '4', '4', '3', '3', '1', '3', '2' ] 
     },
     {
       name: 'Emma Stone',
-      photo: 'https://goo.gl/SScnsS',
+      photo: 'http://bit.ly/2EIA4X2',
       scores: [ '2', '1', '3', '3', '5', '2', '2', '2', '2', '4', '2', '4' ] 
     },
     {
       name: 'Taylor Swift',
-      photo: 'https://goo.gl/dgttwF',
+      photo: 'http://bit.ly/2CAdrhw',
       scores: [ '2', '3', '4', '3', '5', '3', '3', '1', '2', '4', '2', '2' ] 
     },
     {
       name: 'Jennifer Aniston',
-      photo: 'https://goo.gl/JGbHGF',
+      photo: 'http://bit.ly/2sJlWXS',
       scores: [ '2', '1', '5', '3', '5', '1', '1', '1', '2', '4', '1', '1' ] 
     },
     {
       name: 'Julia Roberts',
-      photo: 'https://goo.gl/cxdyaJ',
+      photo: 'http://bit.ly/2uU6Dbl',
       scores: [ '2', '1', '3', '3', '3', '2', '4', '4', '4', '3', '4', '4' ] 
     },
     {
       name: 'Adele',
-      photo: 'https://goo.gl/1VjTuy',
+      photo: 'http://bit.ly/2Hv9TAC',
       scores: [ '2', '1', '2', '3', '2', '2', '4', '4', '3', '1', '4', '3' ] 
     },
     {
       name: 'Chris Pratt',
-      photo: 'https://goo.gl/qYfK9F',
+      photo: 'http://bit.ly/2omjBwq',
       scores: [ '1', '2', '4', '1', '4', '2', '3', '1', '1', '4', '4', '5' ] 
     },
     {
       name: 'Justin Bieber',
-      photo: 'https://goo.gl/yZkazo',
+      photo: 'http://bit.ly/2qQC18v',
       scores: [ '1', '1', '3', '3', '4', '4', '3', '2', '3', '3', '2', '2' ] 
     },
     {
       name: 'Adam Sandler',
-      photo: 'https://goo.gl/he6Cjn',
+      photo: 'http://bit.ly/2i0umRQ',
       scores: [ '1', '2', '2', '3', '4', '1', '2', '2', '4', '3', '2', '2' ] 
     },
     {
       name: 'Drake',
-      photo: 'https://goo.gl/kPPXK2',
+      photo: 'http://bit.ly/2Fg2DYJ',
       scores: [ '1', '2', '2', '4', '2', '3', '4', '4', '5', '3', '2', '2' ] 
     },
     {
       name: 'Ashton Kutcher',
-      photo: 'https://goo.gl/2VXr6n',
+      photo: 'http://bit.ly/2dPecZF',
       scores: [ '1', '3', '5', '2', '5', '2', '1', '1', '1', '4', '1', '1' ] 
     },
     {
-      name: 'Jim Carey',
-      photo: 'https://goo.gl/dgGieH',
+      name: 'Jim Carrey',
+      photo: 'http://bit.ly/2oeNkYW',
       scores: [ '1', '2', '3', '3', '4', '2', '5', '3', '2', '2', '2', '2' ] 
     },
     {
       name: 'Ben Affleck',
-      photo: 'https://goo.gl/C4x2Wt',
+      photo: 'http://bit.ly/2Fh1ksy',
       scores: [ '1', '2', '2', '3', '5', '2', '1', '1', '3', '3', '2', '1' ] 
     }
   ];
